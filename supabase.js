3010dddd8fab1b5e2417aff292d677364a7f7fea
brisingr-eldest <jarrett.mielke@gmail.com>
2025-08-06 11:10:26 -0400

    Initial commit

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://ctndekasxeucndrkbplu.supabase.co',
+  'sb_publishable_anOfYOgEBYQJ8SE2Jfl6sg_rFB_jnJq'
+)
